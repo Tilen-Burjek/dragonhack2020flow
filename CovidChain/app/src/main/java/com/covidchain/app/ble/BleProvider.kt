211a58db8be4ffe24552663cc5138c2d46d3c45b
uramer <antonuramer@gmail.com>
2020-11-07 20:37:09 +0100

    BLE Provider

---
+++ CovidChain/app/src/main/java/com/covidchain/app/ble/BleProvider.kt
@@ -0,0 +1,40 @@
+package com.covidchain.app.ble
+
+import android.app.Service
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+class GattCallback(private val c: Continuation<String>): BluetoothGattCallback() {
+    override fun onCharacteristicRead(
+        gatt: BluetoothGatt,
+        characteristic: BluetoothGattCharacteristic,
+        status: Int
+    ) {
+        if (status == BluetoothGatt.GATT_SUCCESS) {
+            c.resume(characteristic.getStringValue(0))
+        }
+    }
+}
+
+class BleProvider(private val context: Context) {
+    private val TAG = BluetoothLeService::class.java.simpleName
+    private val bluetoothManager = context.getSystemService(Service.BLUETOOTH_SERVICE) as BluetoothManager
+    private val bluetoothAdapter = bluetoothManager.adapter
+
+    suspend fun readCharacteristic(address: String): String {
+        val device = bluetoothAdapter.getRemoteDevice(address)
+        return suspendCoroutine { continuation ->
+            device.connectGatt(context, false, GattCallback(continuation))
+        }
+    }
+
+    private val mGattCallback: BluetoothGattCallback = object : BluetoothGattCallback() {
+
+    }
+}
