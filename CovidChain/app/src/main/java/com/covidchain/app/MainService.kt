11b65c5fa6e03ff27d27a9f430f8c4083cdc2294
uramer <antonuramer@gmail.com>
2020-11-07 15:09:19 +0100

    Main service

---
+++ CovidChain/app/src/main/java/com/covidchain/app/MainService.kt
@@ -0,0 +1,20 @@
+package com.covidchain.app
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.covidchain.app.db.ContactRepository
+import com.covidchain.app.db.DatabaseProvider
+
+class MainService: Service() {
+    lateinit var contactRepository: ContactRepository
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        contactRepository = ContactRepository(DatabaseProvider(this))
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
