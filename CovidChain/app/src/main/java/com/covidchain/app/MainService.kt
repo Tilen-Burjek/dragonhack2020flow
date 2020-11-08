0b7a28a271ce4f91bc93609ac1cf0c9bd271e224
uramer <antonuramer@gmail.com>
2020-11-08 08:46:07 +0100

    Add logo

--- CovidChain/app/src/main/java/com/covidchain/app/MainService.kt
+++ CovidChain/app/src/main/java/com/covidchain/app/MainService.kt
@@ -3,9 +3,14 @@ package com.covidchain.app
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import com.covidchain.app.db.ContactRepository
 import com.covidchain.app.db.DatabaseProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 const val HEALTHY = 0
 const val SICK = 1
@@ -13,11 +18,25 @@ const val SICK = 1
 class MainService: Service() {
     lateinit var contactRepository: ContactRepository
     lateinit var notifier: Notifier
+    lateinit var handler: Handler
 
     override fun onCreate() {
         super.onCreate()
         notifier = Notifier(this)
         contactRepository = ContactRepository(DatabaseProvider(this))
+        handler = Handler(Looper.getMainLooper())
+    }
+
+    private val runnable = Runnable() {
+        fun run() {
+            GlobalScope.launch {
+                //ble.scan
+                val list = listOf<String>("1", "2")
+                list.forEach {
+                    contactRepository.insert(it)
+                }
+            }
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
