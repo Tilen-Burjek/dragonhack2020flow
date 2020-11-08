3f7f7fe0e2b39e6f47ae0bdecb6e6e46a7862ab9
uramer <antonuramer@gmail.com>
2020-11-08 07:57:48 +0100

    Start preparing JSON for http requests

--- CovidChain/app/src/main/java/com/covidchain/app/StatusApi.kt
+++ CovidChain/app/src/main/java/com/covidchain/app/StatusApi.kt
@@ -2,13 +2,16 @@ package com.covidchain.app
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.json.JSONObject
 import java.net.HttpURLConnection
 import java.net.URL
 
+const val BASE_URL = "http://0.0.0.0:80/"
+data class KeyPair(val publicKey: String, val privateKey: String)
 class StatusApi {
     private suspend fun request(url: String, method: String, body: String?): String {
         val result = withContext(Dispatchers.IO) {
-            val conn: HttpURLConnection = URL(url).openConnection() as HttpURLConnection
+            val conn: HttpURLConnection = URL(BASE_URL + url).openConnection() as HttpURLConnection
             conn.requestMethod = method
             body?.let {
                 conn.outputStream.write(body.toByteArray())
@@ -29,11 +32,19 @@ class StatusApi {
         return request(url, "POST", body)
     }
 
-    suspend fun publish(publicKey: String, privateKey: String, sick: Boolean) {
-        //TODO
+    suspend fun getKeys(): KeyPair {
+        val result = JSONObject(get("getnewaddress"))
+        return KeyPair(result.getString("pubkey"), result.getString("privkey"))
     }
 
-    suspend fun receive(publicKey: String) {
-        //TODO
+    suspend fun publish(keys: KeyPair, target: String): String {
+        val body = JSONObject()
+        body.put("address", target)
+        body.put("privkey", keys.privateKey)
+        return post("sendtoaddress/" + keys.publicKey, body.toString())
+    }
+
+    suspend fun receive(publicKey: String): String {
+        return get("receive")
     }
 }
