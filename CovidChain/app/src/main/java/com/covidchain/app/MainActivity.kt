f847077a9ce7bbb076c5f976276dfb6d5cc25779
uramer <antonuramer@gmail.com>
2020-11-08 07:12:11 +0100

    Minimal UI

--- CovidChain/app/src/main/java/com/covidchain/app/MainActivity.kt
+++ CovidChain/app/src/main/java/com/covidchain/app/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.covidchain.app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var statusButton: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        statusButton = findViewById(R.id.statusButton)
+
+        status = loadStatus()
+    }
+
+    private var status: Int = HEALTHY
+        set(value) {
+            field = value
+            updateButton()
+            saveStatus(value)
+        }
+
+    private fun loadStatus(): Int {
+        val pref = getSharedPreferences("CovidChain", Context.MODE_PRIVATE)
+        return pref.getInt("Status", HEALTHY)
+    }
+
+    private fun saveStatus(newStatus: Int) {
+        getSharedPreferences("CovidChain", Context.MODE_PRIVATE)
+            .edit().putInt("Status", newStatus)
+            .apply()
+    }
+
+    fun toggleStatus(view: View) {
+        status = 1 - status
+    }
+
+    private fun updateButton() {
+        if (status == SICK) statusButton.text = getString(R.string.sick)
+        else if (status == HEALTHY) statusButton.text = getString(R.string.healthy)
     }
 }
--- CovidChain/app/src/main/java/com/covidchain/app/MainService.kt
+++ CovidChain/app/src/main/java/com/covidchain/app/MainService.kt
@@ -1,10 +1,14 @@
 package com.covidchain.app
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import com.covidchain.app.db.ContactRepository
 import com.covidchain.app.db.DatabaseProvider
+
+const val HEALTHY = 0
+const val SICK = 1
 
 class MainService: Service() {
     lateinit var contactRepository: ContactRepository
@@ -17,4 +21,6 @@ class MainService: Service() {
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
+
+
 }
